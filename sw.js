97806420f8c6f8e282fda154d989718fba5ca389
hammad57 <46881805+hammad57@users.noreply.github.com>
2026-02-14 01:01:26 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-const cacheName = 'mart-v2';
-const assets = [
-  './',
-  './index.html',
-  'https://unpkg.com/html5-qrcode',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  // Yeh file abhi sirf basic installation ke liye hai
+  e.respondWith(fetch(e.request));
 });
